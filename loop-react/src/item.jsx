fab5a8a267b53a79c5cb5302637e9dcc8425ca6e
lucas <lucas.ssantos189@senacsp.edu.br>
2024-10-29 22:02:14 -0300

    tarrefas

---
+++ loop-react/src/item.jsx
@@ -0,0 +1,21 @@
+import {Fragment } from 'react';
+import Lista from './lista.jsx';
+
+
+function item({nome}) {
+    return (
+        <>
+            <div>
+                nome: {nome} <br/>
+                <button>Editar</button>
+                <button>Remover</button>
+            </div> 
+            <br/>
+            
+
+
+        </>
+    )
+}
+
+export default item;
